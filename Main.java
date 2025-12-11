a4284d5bb4ea79f3d41b9f8205ce59bd79820f6b
juli4nn3 <julianne.leblancpeltier@gmail.com>
2025-12-11 22:52:52 +0000

    progress..?

--- Main.java
+++ Main.java
@@ -9,9 +9,10 @@
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.lang.Math;
 
-final class Main {
-        private Main() {
+final public class Main {
+    private Main() {
         // Prevent instantiation
         // Optional: throw an exception e.g. AssertionError
         // if this ever *is* called
@@ -19,7 +20,7 @@ final class Main {
     }
 
     /** public variables */
-    public static final char[][] phoneLetters = { {"0"}, {"0"}, {"A", "B", "C"}, {"D", "E", "F"}, {"G", "H", "I"}, {"J", "K", "L"}, {"M", "N", "O"}, {"P", "Q", "R", "S"}, {"T", "U", "V"}, {"W", "X", "Y", "Z"}};
+    final char[][] phoneLetters = { {'0'}, {'0'}, {'A', 'B', 'C'}, {'D', 'E', 'F'}, {'G', 'H', 'I'}, {'J', 'K', 'L'}, {'M', 'N', 'O'}, {'P', 'Q', 'R', 'S'}, {'T', 'U', 'V'}, {'W', 'X', 'Y', 'Z'}};
     ArrayList<String> combinations = new ArrayList<String>();
     ArrayList<Integer> index = new ArrayList<Integer>();
 
@@ -29,20 +30,19 @@ final class Main {
         String currentCombination = "";
          
         // if the total possible combinations is equal to the index of the array list "combinations", then return to print all combinations.
-        if (pow(phoneNumber.length(), phoneNumber.length()) == combinations.length()) {
+        if (Math.pow(phoneNumber.length(), phoneNumber.length()) == combinations.length()) {
             return combinations;
         } else {
             // print combination x for all y char in phoneNumber
             // i = current position in index.
             for (int i = 0; i < index.length(); i++) {
                 // sets int var number to equal the value of the current int in phoneNumber
-                int number = phoneNumber[i];
+                int number = phoneNumber.charAt(i);
                 // sets currentCombination to the value of phoneLetters by identifying which number of phoneNumber it is, then by the lowk no clue lol
                 currentCombination += phoneLetters(number, index[i]);
                 index.set(i, index[i] + 1);
             }
             combinations.add(currentCombination);
-            }
             listMnemonics(phoneNumber, index);
         }
     }
@@ -53,7 +53,7 @@ final class Main {
         Scanner userInput = new Scanner(System.in);
         System.out.println("This program generates all possible letter combinations corresponding to numbers on a phone number from an inputted int number string.");
 
-        while (inputError) {
+        while (inputError == true) {
             System.out.println("Enter Phone Number: ");
             String stringOfNumbers = userInput.nextLine();
             try {
