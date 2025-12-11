356ca94995b8ec485befbe1f4b8f69522f30e8c2
juli4nn3 <julianne.leblancpeltier@gmail.com>
2025-12-11 21:29:46 +0000

    i think im making progress???

--- Main.java
+++ Main.java
@@ -8,58 +8,47 @@
 */
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 final class Main {
-    private Main() {
+        private Main() {
         // Prevent instantiation
         // Optional: throw an exception e.g. AssertionError
         // if this ever *is* called
         throw new IllegalStateException("Cannot be instantiated");
     }
 
-    /** variables */
+    /** public variables */
     public static final char[][] phoneLetters = { {"0"}, {"0"}, {"A", "B", "C"}, {"D", "E", "F"}, {"G", "H", "I"}, {"J", "K", "L"}, {"M", "N", "O"}, {"P", "Q", "R", "S"}, {"T", "U", "V"}, {"W", "X", "Y", "Z"}};
     ArrayList<String> combinations = new ArrayList<String>();
+    ArrayList<Integer> index = new ArrayList<Integer>();
 
-    public static ArrayList listMnemonics(final int phoneNumber, int counter, int counter2) {
-        // generate possible letter combinations // calculate total possibilites & counter
+    public static ArrayList listMnemonics(final int phoneNumber, ArrayList<Integer> index) {
+        // generate possible letter combinations
+        // calculate total possibilites & counter
         String currentCombination = "";
          
         // if the total possible combinations is equal to the index of the array list "combinations", then return to print all combinations.
         if (pow(phoneNumber.length(), phoneNumber.length()) == combinations.length()) {
             return combinations;
         } else {
-            // counter for position(#) of char in phoneNumber
-            for (int i = 0; i < phoneNumber.length(); i++) {
-                // sets index as the i char in phoneNumber
-                int index = phoneNumber.charAt(i);
-
-                combinations += phoneLetters(index, i);
-
-
-                // counts position(#) of elements in 2nd array
-                for (int[] innerArray : phoneNumber) {
-                    // current position in array
-                    int count2D += innerArray.length;
-                    currentCombination += innerArray(count2D);
-                    combinations.add(currentCombination);
-                }
-
-                combinations.add
-                listMnemonics(phoneNumber, counter, counter2);
-
+            // print combination x for all y char in phoneNumber
+            // i = current position in index.
+            for (int i = 0; i < index.length(); i++) {
+                // sets int var number to equal the value of the current int in phoneNumber
+                int number = phoneNumber[i];
+                // sets currentCombination to the value of phoneLetters by identifying which number of phoneNumber it is, then by the lowk no clue lol
+                currentCombination += phoneLetters(number, index[i]);
+                index.set(i, index[i] + 1);
             }
-
+            combinations.add(currentCombination);
+            }
+            listMnemonics(phoneNumber, index);
         }
-
-
-
     }
 
     public static void main(final String[] args) {
         // main stub, get user input here
-        int counter = 0;
-        int counter2 = 0;
         boolean inputError = true;
         Scanner userInput = new Scanner(System.in);
         System.out.println("This program generates all possible letter combinations corresponding to numbers on a phone number from an inputted int number string.");
@@ -69,16 +58,21 @@ final class Main {
             String stringOfNumbers = userInput.nextLine();
             try {
                 int numbersAsInt = Integer.parseInt(stringOfNumbers);
+                inputError = false;
             } catch (NumberFormatException e) {
                 System.out.printf("Error! Please enter a valid input!");
             }
-            inputError = false;
         }
 
-        ArrayList<String> letterResults = listMnemonics(numbersAsInt, counter, counter2);
+        for (int i = 0; i < numbersAsInt.length(); i++) {
+            // adds multiple 0's to index, creates counter for all possible combinations.
+            index.add(i, 0);
+        }
+        ArrayList<String> letterResults = listMnemonics(numbersAsInt, index);
 
         System.out.println(letterResults);
 
         System.out.println("\nDone! :)");
+        userInput.close();
     }
 }
